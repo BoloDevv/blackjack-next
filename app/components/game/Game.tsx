ea78c3265ed8544c419e7318d20bd69f7bf35e8c
leomachadods <leomachadods@gmail.com>
2025-03-25 01:12:01 -0300

    Modificar cor do botão 'jogar agora' para 'text-black'

--- app/components/game/Game.tsx
+++ app/components/game/Game.tsx
@@ -15,10 +15,10 @@ export const fontaic = Work_Sans({
 function Game() {
     const [gameStarted, setGameStarted] = useState(false);
     const [playerQuantity, setPlayerQuantity] = useState(1);
-    const {createGameState, hit, stand, players, dealer, isGameOver,isGameWin, resetGameState} = useGameState()
+    const {createGameState, hit, stand, players, dealer, isGameOver, isGameWin, resetGameState} = useGameState()
     const uninitializedPlayers: Player[] = []
 
-        function startGame() {
+    function startGame() {
         for (let i = 0; i < playerQuantity; i++) {
             uninitializedPlayers.push({id: `${i + 1}`, hand: [], name: `player${i + 1}`})
         }
@@ -30,21 +30,24 @@ function Game() {
     return (
         <div className="min-h-screen">
             {gameStarted ? (
-                    <VisualGame hit={hit} stand={stand} players={players} dealer={dealer} isGameOver={isGameOver} isGameWin={isGameWin} resetGame={resetGameState}/>
-            ): (
+                <VisualGame hit={hit} stand={stand} players={players} dealer={dealer} isGameOver={isGameOver}
+                            isGameWin={isGameWin} resetGame={resetGameState}/>
+            ) : (
                 <div className="min-h-screen bg-gradient-to-b from-blackjack-board to-green-800 text-white">
                     <div className="min-h-screen relative flex items-center justify-center overflow-hidden">
                         <BackgroundPattern/>
                         <div className="relative z-10 text-center px-4">
                             <h1 className="text-6xl md:text-7xl mb-6 text-transparent bg-clip-text drop-shadow-2xl">
-                                <span className={`text-transparent font-bold bg-clip-text bg-gradient-to-r from-main-btn to-yellow-200 ${fontaic.className}`}>Royal Blackjack</span>
+                                <span
+                                    className={`text-transparent font-bold bg-clip-text bg-gradient-to-r from-main-btn to-yellow-200 ${fontaic.className}`}>Royal Blackjack</span>
                             </h1>
-                            <p className="text-xl md:text-2xl mb-8 text-gray-300 max-w-2xl mx-auto">Sinta a adrenalina do 21 na ponta dos seus dedos. </p>
+                            <p className="text-xl md:text-2xl mb-8 text-gray-300 max-w-2xl mx-auto">Sinta a adrenalina
+                                do 21 na ponta dos seus dedos. </p>
                             <button
-                                className="group relative inline-flex items-center gap-2 px-8 py-4 bg-main-btn hover:bg-yellow-400text-black font-bold text-lg rounded-full transition-all duration-300 transform hover:scale-105 hover:shadow-xl cursor-pointer"
+                                className="group relative inline-flex items-center gap-2 px-8 py-4 bg-main-btn hover:bg-yellow-400text-black font-bold text-lg rounded-full transition-all duration-300 transform hover:scale-105 hover:shadow-xl cursor-pointer text-black"
                                 onClick={() => startGame()}>
                                 <span>Jogar Agora</span>
-                                <PlayCircle className="w-6 h-6 group-hover:animate-pulse" />
+                                <PlayCircle className="w-6 h-6 group-hover:animate-pulse"/>
                             </button>
                         </div>
                     </div>
